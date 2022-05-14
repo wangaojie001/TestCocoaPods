a6c506d1776285441ff297e893bc994acf7ffd72
gaojie.wan <gaojieapp@163.com>
2022-05-14 22:53:49 +0800

    cehsi

---
+++ Example/Pods/Target Support Files/TestCocoaPods/TestCocoaPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestCocoaPods : NSObject
+@end
+@implementation PodsDummy_TestCocoaPods
+@end
---
+++ Example/TestCocoaPods/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestCocoaPods
+//
+//  Created by gaojie.wan on 05/14/2022.
+//  Copyright (c) 2022 gaojie.wan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        
+        
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
